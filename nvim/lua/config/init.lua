d9b8427927254b71166a4a7bdd00e3352bd150ad
usman1515 <usmansiddique1515@gmail.com>
2023-11-25 00:35:06 +0100

    added lazy package manager in nvim config

---
+++ nvim/lua/config/init.lua
@@ -0,0 +1,27 @@
+-- checks if lazy package manager is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- install the latest stable branch of lazy
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+	"folke/which-key.nvim",
+	{"folke/neoconf.nvim", cmd = "Neoconf"},
+	"folke/neodev.nvim",
+})
+
+-- invoke lazy package manager
+require("lazy").setup(plugins, opts)
