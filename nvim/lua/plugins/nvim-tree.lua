3ebe221868ce4ea1c20767617fc58b9e55e5a41b
usman1515 <usmansiddique1515@gmail.com>
2023-12-10 22:11:29 +0100

    added nvim tree plugin

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-tree/nvim-tree.lua", --source plugin
+	lazy = false, -- load every time
+	config = function()
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+			},
+			view = {
+				adaptive_size = true,
+			},
+		})
+	end,
+}
+
