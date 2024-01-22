e1606b26a1724aea0ff48c071345efd490e26098
usman1515 <usmansiddique1515@gmail.com>
2024-01-22 03:27:12 +0100

    updated nvim indentation

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -8,32 +8,17 @@ local highlight = {
     "RainbowCyan",
 }
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	lazy = false,
-	opts = {},
-	space_char_blankline = " ",
-	show_end_of_line = true,
-    show_current_context = true,
-    show_current_context_start = true,
-    options = {
-        list = true,
-        termguicolors = true,
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    lazy = false,
+    opts = {},
+    indent = {
+        highlight = highlight,
+        char = ""
     },
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
+    whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
     },
 }
