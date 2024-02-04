9565812ebbc095a5cbaae83aa614ad15e4880784
usman1515 <usmansiddique1515@gmail.com>
2024-02-05 00:43:12 +0100

    added keymaps in nvim mason config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -11,6 +11,30 @@ return {
 				package_pending = "➜",
 				package_uninstalled = "✗",
 			},
+			keymaps = {
+				-- Keymap to expand a package
+				toggle_package_expand = "<CR>",
+				-- Keymap to install the package under the current cursor position
+				install_package = "i",
+				-- Keymap to reinstall/update the package under the current cursor position
+				update_package = "u",
+				-- Keymap to check for new version for the package under the current cursor position
+				check_package_version = "c",
+				-- Keymap to update all installed packages
+				update_all_packages = "U",
+				-- Keymap to check which installed packages are outdated
+				check_outdated_packages = "C",
+				-- Keymap to uninstall a package
+				uninstall_package = "X",
+				-- Keymap to cancel a package installation
+				cancel_installation = "<C-c>",
+				-- Keymap to apply language filter
+				apply_language_filter = "<C-f>",
+				-- Keymap to toggle viewing package installation log
+				toggle_package_install_log = "<CR>",
+				-- Keymap to toggle the help view
+				toggle_help = "g?",
+			},
 		},
 		pip = {
 			-- upgrade pip to latest ver in venv before installing packages.
@@ -18,6 +42,5 @@ return {
 			-- these args will be added to `pip install` calls.
 			install_args = {},
 		},
-
 	},
 }
