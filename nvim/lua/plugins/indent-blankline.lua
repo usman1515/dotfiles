b15f737f3253f7fa889db592afdc5d370d547165
usman1515 <usmansiddique1515@gmail.com>
2024-02-23 00:56:04 +0100

    migrated to ver 2.20.8 and added rainbow indents

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,3 +1,5 @@
+local opt = vim.opt
+
 local highlight = {
     "RainbowRed",
     "RainbowYellow",
@@ -10,6 +12,7 @@ local highlight = {
 
 return {
     "lukas-reineke/indent-blankline.nvim",
+    version = "v2.20.8",
     main = "ibl",
     lazy = false,
     opts = {},
@@ -21,4 +24,33 @@ return {
         highlight = highlight,
         remove_blankline_trail = false,
     },
+    config = function()
+        vim.opt.list = true
+        vim.opt.termguicolors = true
+        -- vim.opt.listchars:append "space:⋅"
+        -- vim.opt.listchars:append "eol:↴"
+
+        -- Custom highlight for indent lines
+        vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+        vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+        -- config
+        require("indent_blankline").setup({
+            show_current_context = true,
+            show_current_context_start = true,
+            show_end_of_line = true,
+            space_char_blankline = " ",
+            char_highlight_list = {
+                "IndentBlanklineIndent1",
+                "IndentBlanklineIndent2",
+                "IndentBlanklineIndent3",
+                "IndentBlanklineIndent4",
+                "IndentBlanklineIndent5",
+                "IndentBlanklineIndent6",
+            },
+        })
+    end,
 }
