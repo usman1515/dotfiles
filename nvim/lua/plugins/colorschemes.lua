af60ee906e4f88e15ea4d3f0f70c82e796fe9513
usman1515 <usmansiddique1515@gmail.com>
2024-02-25 01:00:53 +0100

    set bluz71/vim-nightfly-colors as default color scheme

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,22 @@
 -- * disable lazy loading. want theme to be always loaded
 -- * default priority is 50
 
+return {
+    "bluz71/vim-nightfly-colors",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.g.nightflyCursorColor = false
+        vim.g.nightflyItalics = true
+        vim.g.nightflyNormalFloat = false
+        -- set global transparency
+        vim.g.nightflyTransparent = true
+        -- Apply the nightfly colorscheme
+        vim.cmd("colorscheme nightfly")
+    end,
+    opts = {},
+}
+
 -- return {
 --     "folke/tokyonight.nvim",
 --     lazy = false,
@@ -32,31 +48,21 @@
 -- }
 
 -- return {
---     "bluz71/vim-nightfly-colors",
+--     "sainnhe/sonokai",
 --     lazy = false,
 --     priority = 1000,
 --     config = function()
---         vim.cmd("colorscheme nightfly")
+--         -- vim.api.nvim_set_var('sonokai_style', 'default')    -- colorscheme 1
+--         -- vim.api.nvim_set_var('sonokai_style', 'atlantis')   -- colorscheme 2
+--         vim.api.nvim_set_var('sonokai_style', 'andromeda')  -- colorscheme 3
+--         -- vim.api.nvim_set_var('sonokai_style', 'shusia')     -- colorscheme 4
+--         -- vim.api.nvim_set_var('sonokai_style', 'maia')       -- colorscheme 5
+--         -- vim.api.nvim_set_var('sonokai_style', 'espresso')   -- colorscheme 6
+--         -- vim.api.nvim_set_var('sonokai_style', 'maia')       -- colorscheme 7
+--         vim.cmd("colorscheme sonokai")                      -- set theme
+--         vim.api.nvim_set_var('sonokai_better_performance', 1)   -- improve performance
+--         vim.api.nvim_set_var('sonokai_enable_italic', 1) -- enable italic
+--         vim.api.nvim_set_var('sonokai_transparent_background', 2) -- transparency
 --     end,
 --     opts = {},
 -- }
-
-return {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- vim.api.nvim_set_var('sonokai_style', 'default')    -- colorscheme 1
-        -- vim.api.nvim_set_var('sonokai_style', 'atlantis')   -- colorscheme 2
-        vim.api.nvim_set_var('sonokai_style', 'andromeda')  -- colorscheme 3
-        -- vim.api.nvim_set_var('sonokai_style', 'shusia')     -- colorscheme 4
-        -- vim.api.nvim_set_var('sonokai_style', 'maia')       -- colorscheme 5
-        -- vim.api.nvim_set_var('sonokai_style', 'espresso')   -- colorscheme 6
-        -- vim.api.nvim_set_var('sonokai_style', 'maia')       -- colorscheme 7
-        vim.cmd("colorscheme sonokai")                      -- set theme
-        vim.api.nvim_set_var('sonokai_better_performance', 1)   -- improve performance
-        vim.api.nvim_set_var('sonokai_enable_italic', 1) -- enable italic
-        vim.api.nvim_set_var('sonokai_transparent_background', 2) -- transparency
-    end,
-    opts = {},
-}
