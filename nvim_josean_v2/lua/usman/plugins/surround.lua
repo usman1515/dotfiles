eaef81e1e8aac1350f5ebe64fca044c6f44e8bd4
usman1515 <usmansiddique1515@gmail.com>
2024-08-18 02:05:25 +0200

    nvim plugin added for add, remove and substitue symbols: kylechui/nvim-surround

---
+++ nvim_josean_v2/lua/usman/plugins/surround.lua
@@ -0,0 +1,13 @@
+-- can be used to add, remove and substitute single, double quotes and brackets
+-- to add quotes: ys + iw + ""
+-- to remove quotes: ds + ""
+-- to change symbol: cs + old_symbol + new_symbol
+
+
+
+return {
+    "kylechui/nvim-surround",
+    event = { "BufReadPre", "BufNewFile" },
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = true,
+}
