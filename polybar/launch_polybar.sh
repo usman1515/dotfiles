b0960e8a459ad7653a18f1b9162addf3b93fd60f
usman1515 <usmansiddique1515@gmail.com>
2025-05-01 23:53:41 +0200

    fix: renamed script. now supports 2 bars for 2 monitors

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -6,7 +6,12 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
+
+# launch 1 bar
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar bar_config1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# launch 2 bars
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar_config1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar_config2 2>&1 | tee -a /tmp/polybar2.log & disown
