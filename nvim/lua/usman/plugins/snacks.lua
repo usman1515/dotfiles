7c871ae638607feca0b62a23be696272f081d320
usman1515 <usmansiddique1515@gmail.com>
2025-05-14 00:57:02 +0200

    feat, refactor: multiple changes in snacks.nvim
    removed file formats from snacks.image
    disabled math support from snacks.image
    added snacks.indent, snacks.quickfile
    updated snacks.notifier, snacks.toggle, snacks.keymaps
    moved snacks.scope inside snacks.indent

--- nvim/lua/usman/plugins/snacks.lua
+++ nvim/lua/usman/plugins/snacks.lua
@@ -59,7 +59,7 @@ return {
             title_pos = "center",
             ft = "git",
         },
-        image = {                           --  view images in nvim snacks.picker
+        image = {                           -- view images in nvim snacks.picker
             enabled = true,
             formats = {
                 "png",
@@ -72,12 +72,6 @@ return {
                 "tiff",
                 "heic",
                 "avif",
-                "mp4",
-                "mov",
-                "avi",
-                "mkv",
-                "webm",
-                "pdf",
             },
             force = false, -- try displaying the image, even if the terminal does not support it
             img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments", "figures" },
@@ -124,40 +118,71 @@ return {
                 },
             },
             math = {
-                enabled = true, -- enable math expression rendering
-                -- in the templates below, `${header}` comes from any section in your document,
-                -- between a start/end header comment. Comment syntax is language-specific.
-                -- * start comment: `// snacks: header start`
-                -- * end comment:   `// snacks: header end`
-                typst = {
-                    tpl = [[
-                    #set page(width: auto, height: auto, margin: (x: 2pt, y: 2pt))
-                    #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
-                    #set text(size: 12pt, fill: rgb("${color}"))
-                    ${header}
-                    ${content}]],
+                enabled = false, -- enable math expression rendering
+            },
+        },
+        indent = {                          -- visualize indent guides and scopes
+            enabled = true, -- enable indent guides
+            -- your indent configuration goes here
+            indent = {
+                priority = 1,
+                char = "│", -- "┊" "▎" "│"
+                only_scope = false, -- only show indent guides of the scope
+                only_current = false, -- only show indent guides in the current window
+                hl = "SnacksIndent", ---@type string|string[] hl groups for indent guides
+                -- can be a list of hl groups to cycle through
+                hl = {
+                    "SnacksIndent1",
+                    "SnacksIndent2",
+                    "SnacksIndent3",
+                    "SnacksIndent4",
+                    "SnacksIndent5",
+                    "SnacksIndent6",
+                    "SnacksIndent7",
+                    "SnacksIndent8",
                 },
-                latex = {
-                    font_size = "Large", -- see https://www.sascha-frank.com/latex-font-size.html
-                    -- for latex documents, the doc packages are included automatically,
-                    -- but you can add more packages here. Useful for markdown documents.
-                    packages = { "amsmath", "amssymb", "amsfonts", "amscd", "mathtools" },
-                    tpl = [[
-                    \documentclass[preview,border=0pt,varwidth,12pt]{standalone}
-                    \usepackage{${packages}}
-                    \begin{document}
-                    ${header}
-                    { \${font_size} \selectfont
-                    \color[HTML]{${color}}
-                    ${content}}
-                    \end{document}]],
+            },
+            animate = {
+                enabled = vim.fn.has("nvim-0.10") == 1,
+                style = "out",
+                easing = "linear",
+                duration = {
+                    step = 20, -- ms per step
+                    total = 500, -- maximum duration
+                },
+            },
+            scope = {
+                enabled = true, -- enable highlighting the current scope
+                priority = 200,
+                char = "│",
+                underline = false, -- underline the start of the scope
+                only_current = false, -- only show scope in the current window
+                hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+            },
+            chunk = {
+                -- when enabled, scopes will be rendered as chunks, except for the
+                -- top-level scope which will be rendered as a scope.
+                enabled = true,
+                -- only show chunk scopes in the current window
+                only_current = false,
+                priority = 200,
+                hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+                char = {
+                    -- corner_top = "┌",
+                    -- corner_bottom = "└",
+                    corner_top = "╭",
+                    corner_bottom = "╰",
+                    horizontal = "─",
+                    vertical = "│",
+                    arrow = ">",
                 },
             },
         },
         notifier = {                        -- notifications UI
             enabled = true,
             -- your notifier configuration goes here
-            timeout = 3000, -- default timeout in ms
+            timeout = 5000, -- default timeout in ms
+            padding = true, -- add 1 cell of left/right padding to the notification window
             sort = { "level", "added" }, -- sort by level and time
             level = vim.log.levels.TRACE,
             icons = {
@@ -167,6 +192,7 @@ return {
                 debug = " ",
                 trace = " ",
             },
+            style = "compact",
             date_format = "%R", -- time format for notifications
             more_format = " ↓ %d lines ",
             refresh = 50,   -- refresh at most every 50ms
@@ -175,9 +201,10 @@ return {
             enabled = true,
             -- your picker configuration comes here
         },
-        scope = { enabled = true },         -- scope detection based on treesitter
-        statuscolumn = { enabled = true },
-        toggle = {                          -- toggle kepmaps
+        quickfile = { enabled = true },     -- render file as quickly as possible
+        scroll = { enabled = true },        -- smoother scrolling
+        statuscolumn = { enabled = true },  --
+        toggle = {                          -- toggle keymap clues
             enabled = true,
             -- your toggle configuration comes here
             map = vim.keymap.set, -- keymap.set function to use
@@ -193,28 +220,37 @@ return {
                 enabled = "green",
                 disabled = "yellow",
             },
+            wk_desc = {
+                enabled = "Disable ",
+                disabled = "Enable ",
+            },
         },
     },
-    -- optional = true,
     keys = {
         -- INFO: essential keymaps
-        { "<leader>space>", function() Snacks.picker.files() end, desc = "Find Files" },
+        { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
         { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
         { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
         { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-        { "<leader>e", function() Snacks.picker.explorer() end, desc = "Toggle File Tree Explorer" },
+        { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+        { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
         -- find
-        { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
         { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
         { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+        { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
         { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
         { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
         -- todo comments
-        { "<leader>st", function () Snacks.picker.todo_comments({ keywords = { "TODO", "HACK", "WARNING", "BUG", "NOTE", "INFO", "PERF", "ERROR" } }) end, desc = "Todo Comment Tags" },
-        { "<leader>sT", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+        { "<leader>st", function() Snacks.picker.todo_comments({ keywords = { "TODO", "HACK", "WARNING", "BUG", "NOTE", "INFO", "PERF", "ERROR" } }) end, desc = "Todo Comment Tags" },
         -- git
-        { "<leader>gc", function() Snacks.picker.git_log() end, desc = "Git Log" },
+        { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+        { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+        { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
         { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+        { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+        { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+        { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
         -- grep
         { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
         { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
@@ -222,27 +258,45 @@ return {
         { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
         -- search
         { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+        { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
         { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+        { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
         { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
         { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
         { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+        { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
         -- { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
         { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+        { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
         { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
         { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
         { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
-        { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
         { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
-        { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+        { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+        { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
         { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+        { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+        { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
         { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
-        { "<leader>qp", function() Snacks.picker.projects() end, desc = "Projects" },
         -- LSP
-        -- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-        -- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-        -- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-        -- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-        -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+        { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+        { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+        { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+        { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+        -- Other
+        { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+        { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+        { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+        { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+        -- { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+        -- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+        -- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+        -- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     },
 }
 
