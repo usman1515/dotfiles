53f41550e1ad7db67e51428d043b08df106037ca
usman1515 <usmansiddique1515@gmail.com>
2025-06-29 18:17:36 +0200

    fix: minor changes. added a gruvbox theme

--- nvim/lua/usman/plugins/colorschemes.lua
+++ nvim/lua/usman/plugins/colorschemes.lua
@@ -1,90 +1,69 @@
 -- INFO: disable lazy loading. want theme to be always loaded
 -- default priority is 50
 
-return {
-    {
-        "oxfist/night-owl.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("night-owl").setup({
-                -- These are the default settings
-                bold = true,
-                italics = true,
-                underline = true,
-                undercurl = true,
-                transparent_background = false,
-            })
-            vim.cmd("colorscheme night-owl")
-        end,
-    },
-    {
-        "Mofiqul/vscode.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function ()
-            -- vim.cmd("colorscheme vscode")
-        end,
-        opts = {},
-    },
-    {
-        "bluz71/vim-nightfly-colors",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.nightflyCursorColor = true
-            vim.g.nightflyItalics = true
-            vim.g.nightflyNormalFloat = false
-            -- set global transparency
-            vim.g.nightflyTransparent = false
-            -- Apply the nightfly colorscheme
-            -- vim.cmd("colorscheme nightfly")
-        end,
-        opts = {},
-    },
-    {
-        "bluz71/vim-moonfly-colors",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.moonflyCursorColor = true
-            vim.g.moonflyItalics = true
-            vim.g.moonflyNormalFloat = false
-            -- set global transparency
-            vim.g.moonflyTransparent = false
-            -- Apply the moonfly colorscheme
-            -- vim.cmd("colorscheme moonfly")
-        end,
-        opts = {},
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- colorschemes for the different styles. use one only
-            -- vim.cmd("colorscheme tokyonight-storm") --dark
-            -- vim.cmd("colorscheme tokyonight-night") --dark
-            -- vim.cmd("colorscheme tokyonight-moon")  --dark
-            -- vim.cmd("colorscheme tokyonight-day")   --light
-        end,
-        opts = {},
-    },
-    {
-        "EdenEast/nightfox.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- vim.cmd("colorscheme dayfox")   -- light
-            -- vim.cmd("colorscheme dawnfox")  -- light
-            -- vim.cmd("colorscheme nightfox") -- dark
-            -- vim.cmd("colorscheme duskfox")  -- dark
-            -- vim.cmd("colorscheme nordfox")  -- dark
-            -- vim.cmd("colorscheme terafox")  -- dark
-            -- vim.cmd("colorscheme carbonfox")    -- dark
-        end,
-        opts = {},
-    },
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         -- colorschemes for the different styles. use one only
+--         vim.cmd("colorscheme tokyonight-storm") --dark
+--         -- vim.cmd("colorscheme tokyonight-night") --dark
+--         -- vim.cmd("colorscheme tokyonight-moon")  --dark
+--         -- vim.cmd("colorscheme tokyonight-day")   --light
+--     end,
+--     opts = {},
+-- }
 
+return {
+    "bluz71/vim-nightfly-colors",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.g.nightflyCursorColor = true
+        vim.g.nightflyItalics = true
+        vim.g.nightflyNormalFloat = false
+        vim.g.nightflyTerminalColors = true
+        vim.g.nightflyTransparent = false
+        vim.g.nightflyUndercurls = true
+        vim.g.nightflyVirtualTextColor = true
+        vim.g.nightflyWinSeparator = 2
+        vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳',
+            vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋',
+        }
+        -- Apply the nightfly colorscheme
+        vim.cmd("colorscheme nightfly")
+    end,
+    opts = {}
 }
 
+-- return {
+--     "ellisonleao/gruvbox.nvim",
+--     lazy = false,
+--     priority = 1000 ,
+--     config = function()
+--         vim.g.terminal_colors = true   -- add neovim terminal colors
+--         vim.g.undercurl = true
+--         vim.g.underline = true
+--         vim.g.bold = true
+--         vim.g.italic = {
+--             strings = true,
+--             emphasis = true,
+--             comments = true,
+--             operators = false,
+--             folds = true,
+--         }
+--         vim.g.strikethrough = true
+--         vim.g.invert_selection = false
+--         vim.g.invert_signs = false
+--         vim.g.invert_tabline = false
+--         vim.g.inverse = true        -- invert background for search, diffs, statuslines and errors
+--         vim.g.contrast = ""         -- can be "hard", "soft" or empty string
+--         vim.g.palette_overrides = {}
+--         vim.g.overrides = {}
+--         vim.g.dim_inactive = false
+--         vim.g.transparent_mode = false
+--         vim.cmd("colorscheme gruvbox")
+--     end,
+--     opts = {}
+-- }
