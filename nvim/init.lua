62a098103702a8dee2e342796c9c11da1e740216
usman1515 <usmansiddique1515@gmail.com>
2026-06-03 19:22:55 +0500

    feat: added and enabled nvim 0.12 UI2

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
-require("usman.core")
-require("usman.lazy")
+-- enable nvim ui2
+require('vim._core.ui2').enable({
+    enable = true,
+    msg = {
+        target = "cmd", -- options: cmd(classic), msg(similar to noice)
+        pager = { height = 1 },
+        msg   = { height = 0.5, timeout = 4500 },
+        dialog = { height = 0.5 },
+        cmd    = { height = 0.5 },
+    },
+})
 
+require("usman.core")
+require("usman.vimpack")
