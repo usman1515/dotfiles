7fd7e99e0d00c9ca2404da17ea55342aef5a680e
usman1515 <usmansiddique1515@gmail.com>
2026-06-07 15:10:04 +0500

    feat: migrated vimtex plugin config from lazyvim to vimpack

--- nvim/lua/usman/plugins/vimtex.lua
+++ nvim/lua/usman/plugins/vimtex.lua
@@ -1,26 +1,36 @@
 -- INFO: VimtexCompile - start continuous compilation
 -- VimtexStop - stop continuous compilation
 
-return {
-    "lervag/vimtex",
-    lazy = false,     -- Load VimTeX immediately
-    init = function()
-        vim.g.vimtex_view_method = "zathura"
-        -- Configure VimTeX to use latexmk
-        vim.g.vimtex_compiler_latexmk = {
-            executable = "latexmk",
-            options = {
-                "-pdf",                 -- Generate PDF (let latexmk decide the engine)
-                "-shell-escape",        -- Required for TikZ, minted, etc.
-                "-verbose",
-                "-file-line-error",
-                "-interaction=nonstopmode",
-                "-synctex=1",
-                "-outdir=build"         -- Store compiled files in 'build/' directory
-            },
-            continuous = 1,             -- Enable automatic recompile
-            callback = 1,
-            log_file = "build/latexmk_compile.log"  -- Save compile logs
-        }
-    end
+vim.pack.add({ "https://github.com/lervag/vimtex" })
+
+
+
+-- === PDF Viewer ===
+vim.g.vimtex_view_method = "sioyek"     -- macos PDF viewer
+-- vim.g.vimtex_view_method = "zathura"    -- linux PDF viewer
+
+-- === Main LaTeX file ===
+vim.g.vimtex_mainfile = "main.tex"
+
+-- === Choose compilation engine ===
+-- Options: "xelatex" or "lualatex"
+local engine = "lualatex"       -- change to "lualatex" if you want LuaLaTeX
+local engine_flag = "-pdfxe"    -- default XeLaTeX
+if engine == "lualatex" then
+    engine_flag = "-pdflua"
+end
+-- === latexmk compiler options ===
+vim.g.vimtex_compiler_latexmk = {
+    executable = "latexmk",
+    options = {
+        engine_flag,            -- set the engine
+        "-shell-escape",        -- required for TikZ, minted, etc.
+        "-interaction=nonstopmode",
+        "-file-line-error",
+        "-synctex=1",
+        "-outdir=build"         -- compile output directory
+    },
+    continuous = 1,             -- auto-recompile on file save
+    callback = 1,
+    log_file = "build/latexmk_compile.log"
 }
