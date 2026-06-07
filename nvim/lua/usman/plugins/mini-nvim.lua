2221d262cee5d559c89ddf52a6dbd9a92e9e8fc1
usman1515 <usmansiddique1515@gmail.com>
2026-06-07 15:02:43 +0500

    fix: commented mini.diff since i am using gitsigns

--- nvim/lua/usman/plugins/mini-nvim.lua
+++ nvim/lua/usman/plugins/mini-nvim.lua
@@ -79,54 +79,54 @@ require('mini.clue').setup({
 require('mini.cursorword').setup({})
 
 -- INFO: mini.diff - git integration for buffers and hunks
-require('mini.diff').setup({
-    -- Options for how hunks are visualized
-    view = {
-        -- Visualization style. Possible values are 'sign' and 'number'.
-        -- Default: 'number' if line numbers are enabled, 'sign' otherwise.
-        style = vim.go.number and 'number' and 'sign',
-        -- Signs shown in the sign column
-        signs = { add = '▒', change = '▒', delete = '' },      -- mini.diff symbols
-        -- signs = { add = '┃', change = '┃', delete = '' },   -- gitsigns symbols
-        -- Priority of used visualization extmarks
-        priority = 199,
-    },
-    -- Source(s) for how reference text is computed/updated/etc
-    -- Uses content from Git index by default
-    source = nil,
-    -- Delays (in ms) defining asynchronous processes
-    delay = {
-        -- How much to wait before update following every text change
-        text_change = 200,
-    },
-    -- Module mappings. Use `''` (empty string) to disable one.
-    -- Replaced default mini.diff keymaps with gitsigns.nvim keymaps.
-    mappings = {
-        -- Apply hunks inside a visual/operator region
-        apply       = '<leader>hs',
-        -- Reset hunks inside a visual/operator region
-        reset       = '<leader>hr',
-        -- Hunk range textobject to be used inside operator
-        textobject  = 'ih',
-        -- Go to hunk range in corresponding direction
-        goto_first  = '[C',
-        goto_prev   = '[c',
-        goto_next   = ']c',
-        goto_last   = ']C',
-    },
-    -- Various options
-    options = {
-        -- Diff algorithm. See `:h vim.diff()`.
-        algorithm = 'histogram',
-        -- Whether to use "indent heuristic". See `:h vim.diff()`.
-        indent_heuristic = true,
-        -- The amount of second-stage diff to align lines
-        linematch = 60,
-        -- Whether to wrap around edges during hunk navigation
-        wrap_goto = false,
-    },
-})
-
+-- require('mini.diff').setup({
+--     -- Options for how hunks are visualized
+--     view = {
+--         -- Visualization style. Possible values are 'sign' and 'number'.
+--         -- Default: 'number' if line numbers are enabled, 'sign' otherwise.
+--         style = vim.go.number and 'number' and 'sign',
+--         -- Signs shown in the sign column
+--         -- signs = { add = '▒', change = '▒', delete = '' },      -- mini.diff symbols
+--         signs = { add = '┃', change = '┃', delete = '' },   -- gitsigns symbols
+--         -- Priority of used visualization extmarks
+--         priority = 199,
+--     },
+--     -- Source(s) for how reference text is computed/updated/etc
+--     -- Uses content from Git index by default
+--     source = nil,
+--     -- Delays (in ms) defining asynchronous processes
+--     delay = {
+--         -- How much to wait before update following every text change
+--         text_change = 200,
+--     },
+--     -- Module mappings. Use `''` (empty string) to disable one.
+--     -- Replaced default mini.diff keymaps with gitsigns.nvim keymaps.
+--     mappings = {
+--         -- Apply hunks inside a visual/operator region
+--         apply       = '<leader>hs',
+--         -- Reset hunks inside a visual/operator region
+--         reset       = '<leader>hr',
+--         -- Hunk range textobject to be used inside operator
+--         textobject  = 'ih',
+--         -- Go to hunk range in corresponding direction
+--         goto_first  = '[C',
+--         goto_prev   = '[c',
+--         goto_next   = ']c',
+--         goto_last   = ']C',
+--     },
+--     -- Various options
+--     options = {
+--         -- Diff algorithm. See `:h vim.diff()`.
+--         algorithm = 'histogram',
+--         -- Whether to use "indent heuristic". See `:h vim.diff()`.
+--         indent_heuristic = true,
+--         -- The amount of second-stage diff to align lines
+--         linematch = 60,
+--         -- Whether to wrap around edges during hunk navigation
+--         wrap_goto = false,
+--     },
+-- })
+--
 -- mini.diff - custom colors for MiniDiff signs (similar to Gitsigns)
 local function set_minidiff_colors()
     vim.api.nvim_set_hl(0, "MiniDiffSignAdd",    { fg = "#90ee90", bg = "NONE" })    -- Light green
