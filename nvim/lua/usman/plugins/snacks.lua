fec141ee7429d7bde326b91fc32f143a8691386f
usman1515 <usmansiddique1515@gmail.com>
2026-07-05 00:10:45 +0500

    feat: updated snakcs.nvim with latest keymaps

--- nvim/lua/usman/plugins/snacks.lua
+++ nvim/lua/usman/plugins/snacks.lua
@@ -221,8 +221,13 @@ vim.keymap.set("n", "<leader>st", function() Snacks.picker.todo_comments({ keywo
 vim.keymap.set("n", "<leader>sT", function() Snacks.picker.todo_comments() end, { desc = "Todo" })
 
 -- git
--- vim.keymap.set("n", "<leader>gc", function() Snacks.picker.git_log() end, { desc = "Git Log" })
--- vim.keymap.set("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
+vim.keymap.set("n", "<leader>gb", function() Snacks.picker.git_branches() end, { desc = "Git Branches" })
+vim.keymap.set("n", "<leader>gl", function() Snacks.picker.git_log() end, { desc = "Git Log" })
+vim.keymap.set("n", "<leader>gL", function() Snacks.picker.git_log_line() end, { desc = "Git Log Line" })
+vim.keymap.set("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
+vim.keymap.set("n", "<leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git Stash" })
+vim.keymap.set("n", "<leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git Diff ( }Hunks)"})
+vim.keymap.set("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File }"})
 
 -- grep
 vim.keymap.set("n", "<leader>sb", function() Snacks.picker.lines() end, { desc = "Buffer Lines" })
@@ -232,12 +237,15 @@ vim.keymap.set({ "n", "x" }, "<leader>sw", function() Snacks.picker.grep_word() 
 
 -- search
 vim.keymap.set("n", '<leader>s"', function() Snacks.picker.registers() end, { desc = "Registers" })
+vim.keymap.set("n", '<leader>s/', function() Snacks.picker.search_history() end, { desc = "Search History" })
 vim.keymap.set("n", "<leader>sa", function() Snacks.picker.autocmds() end, { desc = "Autocmds" })
 vim.keymap.set("n", "<leader>sc", function() Snacks.picker.command_history() end, { desc = "Command History" })
 vim.keymap.set("n", "<leader>sC", function() Snacks.picker.commands() end, { desc = "Commands" })
 vim.keymap.set("n", "<leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, { desc = "Buffer Diagnostics" })
 -- vim.keymap.set("n", "<leader>sh", function() Snacks.picker.help() end, { desc = "Help Pages" })
 vim.keymap.set("n", "<leader>sH", function() Snacks.picker.highlights() end, { desc = "Highlights" })
+vim.keymap.set("n", "<leader>si", function() Snacks.picker.icons() end, { desc = "Icons" })
 vim.keymap.set("n", "<leader>sj", function() Snacks.picker.jumps() end, { desc = "Jumps" })
 vim.keymap.set("n", "<leader>sk", function() Snacks.picker.keymaps() end, { desc = "Keymaps" })
 vim.keymap.set("n", "<leader>sl", function() Snacks.picker.loclist() end, { desc = "Location List" })
@@ -245,13 +253,31 @@ vim.keymap.set("n", "<leader>sM", function() Snacks.picker.man() end, { desc = "
 vim.keymap.set("n", "<leader>sm", function() Snacks.picker.marks() end, { desc = "Marks" })
 vim.keymap.set("n", "<leader>sR", function() Snacks.picker.resume() end, { desc = "Resume" })
 vim.keymap.set("n", "<leader>sq", function() Snacks.picker.qflist() end, { desc = "Quickfix List" })
+vim.keymap.set("n", "<leader>su", function() Snacks.picker.undo() end, { desc = "Undo History" })
 vim.keymap.set("n", "<leader>uC", function() Snacks.picker.colorschemes() end, { desc = "Colorschemes" })
 vim.keymap.set("n", "<leader>qp", function() Snacks.picker.projects() end, { desc = "Projects" })
 
 -- LSP
--- vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "Goto Definition" })
--- vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { nowait = true, desc = "References" })
--- vim.keymap.set("n", "gI", function() Snacks.picker.lsp_implementations() end, { desc = "Goto Implementation" })
--- vim.keymap.set("n", "gy", function() Snacks.picker.lsp_type_definitions() end, { desc = "Goto T[y]pe Definition" })
--- vim.keymap.set("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
+vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "Goto Definition" })
+vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, desc = { "Goto Declaration"})
+vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { nowait = true, desc = "References" })
+vim.keymap.set("n", "gI", function() Snacks.picker.lsp_implementations() end, { desc = "Goto Implementation" })
+vim.keymap.set("n", "gy", function() Snacks.picker.lsp_type_definitions() end, { desc = "Goto T[y]pe Definition" })
+vim.keymap.set("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
 
+
+-- -- Other
+-- { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+-- { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+-- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+-- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+vim.keymap.set("n", "<leader>n",  function() Snacks.notifier.show_history() end, { desc = "Notification History" })
+-- { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+-- { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+-- { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+-- { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+-- { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+-- { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+-- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+-- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+-- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
